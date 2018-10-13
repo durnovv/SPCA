97c9dd1e404effcc5b0f1d1d0a1b57fd582741ea
durnovv <vad.durnov2010@gmail.com>
2018-10-13 16:11:07 +0200

    Update boxarrow.c

--- Pointers/boxarrow.c
+++ Pointers/boxarrow.c
@@ -7,8 +7,8 @@ int main(int argc, char **argv) {
 
   printf("&x: %p;  x: %d\n", &x, x);
   printf("&arr[0]: %p;  arr[0]: %d\n", &arr[0], arr[0]);
-  printf("&arr[2]: %p;  arr[2]: %d\n", &arr[2], arr[2]);
-  printf("&p: %p; p: %p; *p: %d\n", &p, p, *p);
+  printf("&arr[2]: %p;  arr[2]: %d\n", &arr[2], arr[2]);    
+  printf("&p: %p; p: %p; *p: %d\n", &p, p, *p);           	//*p points to the address where value to print is stored
 
 	  return 0;
 }
